9ba05b79665fb0b8e4af17f185a603efd9885ae3
Donni Richasdy <donni.richasdy@gmail.com>
2016-12-04 11:10:50 +0700

    constant

---
+++ constant.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+let constA = 42
+print(constA)
+
+
+// type anotation
+let constB:Float = 3.14159
+print(constB)
+
+// naming constant
+let _const = "Hello, Swift!"
+print(_const)
+
+let 你好 = "你好世界"
+print(你好)
+
+
+let constC = "Godzilla"
+let constD = 1000.00
+
+print("Value of \(constC) is more than \(constD) millions")
