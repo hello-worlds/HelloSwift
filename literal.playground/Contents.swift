0fd788b88fb49a68d32ead0ff0997116000a1bc5
Donni Richasdy <donni.richasdy@gmail.com>
2016-12-04 11:11:06 +0700

    literal

---
+++ literal.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+let decimalInteger = 17           // 17 in decimal notation
+let binaryInteger = 0b10001       // 17 in binary notation
+let octalInteger = 0o21           // 17 in octal notation
+let hexadecimalInteger = 0x11     // 17 in hexadecimal notation
+print(decimalInteger)
+print(binaryInteger)
+print(octalInteger)
+print(hexadecimalInteger)
+
+let decimalDouble = 12.1875
+let exponentDouble = 1.21875e1
+let hexadecimalDouble = 0xC.3p0
+print(decimalDouble)
+print(exponentDouble)
+print(hexadecimalDouble)
+
+let stringL = "Hello\tWorld\n\nHello\'Swift\'"
+print(stringL)
